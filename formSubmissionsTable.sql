9e81f23cf4642faa9bef52954b0fdb079c3162ed
Aidan Tomcy <aidantomcy@gmail.com>
2021-11-03 11:21:32 +0530

    Update formSubmissionsTable.sql

--- formSubmissionsTable.sql
+++ formSubmissionsTable.sql
@@ -1,3 +1,4 @@
+--Code for the form_submissions table to store contents of contact form in main.php
 create table form_submissions (
     id int(11) primary key auto_increment not null,
     firstName varchar(128) not null,
