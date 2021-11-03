fb020905d85d346454d93e7bb97d554baf21cda9
Aidan Tomcy <aidantomcy@gmail.com>
2021-11-03 11:15:20 +0530

    Update formSubmissionsTable.sql

--- formSubmissionsTable.sql
+++ formSubmissionsTable.sql
@@ -1,4 +1,5 @@
 create table form_submissions (
+    id int(11) primary key auto_increment not null,
     firstName varchar(128) not null,
     lastName varchar(128) not null,
     emailId varchar(128) not null,
