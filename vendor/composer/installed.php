3dc850ec95d12ac099bc1d7b11340064325abff7
Aidan Tomcy <aidantomcy@gmail.com>
2021-11-06 13:07:36 +0530

    Initialized Composer Project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd958a847adacf2d085360bf19bd4b36df99e6e70',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd958a847adacf2d085360bf19bd4b36df99e6e70',
+            'dev_requirement' => false,
+        ),
+    ),
+);
