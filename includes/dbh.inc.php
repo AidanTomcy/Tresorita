a6fe596db7f984b6ddaa6879b159b7da22b80f81
Aidan Tomcy <aidantomcy@gmail.com>
2021-11-10 15:55:26 +0530

    Made variables constants in dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,11 @@
 <?php
   #Database Connection
-  $serverName = "localhost";
-  $dBUsername = "root";
-  $dBPassword = "";
-  $dBName = "users";
+  define("SERVER_NAME", "localhost");
+  define("DB_USER_NAME", "root");
+  define("DB_PASSWORD", "");
+  define("DB_NAME", "users");
 
-  $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+  $conn = mysqli_connect(SERVER_NAME, DB_USER_NAME, DB_PASSWORD, DB_NAME);
 
   if (!$conn) {
     die("Connection failed: " . mysqli_connection_error());
