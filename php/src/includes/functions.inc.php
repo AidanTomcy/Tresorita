d55eeff2b098ef42fe4a4c81fb32bcff3ac1d409
Aidan Tomcy <aidantomcy@gmail.com>
2022-01-05 17:32:21 +0530

    Remove 'storeContentsOfContact' function

--- php/src/includes/functions.inc.php
+++ php/src/includes/functions.inc.php
@@ -131,17 +131,3 @@
         exit();
     }
 }
-
-  function storeContentsOfContact($conn, $firstName, $lastName, $email, $message) {
-    $sql = "INSERT INTO form_submissions (firstName, lastName, emailId, message) VALUES (?, ?, ?, ?);";
-    $stmt = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("location: ../main.index?error=stmtfailed");
-        exit();
-    }
-
-    mysqli_stmt_bind_param($stmt, "ssss", $firstName, $lastName, $email, $message);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_close($stmt);
-    header("location: ../index.php?error=none");
-}
