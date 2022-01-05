0cc6aa6d77e6c7856b7243852649b013355d3791
Aidan Tomcy <aidantomcy@gmail.com>
2022-01-05 17:31:44 +0530

    Update DB name

--- php/src/includes/dbh.inc.php
+++ php/src/includes/dbh.inc.php
@@ -3,7 +3,7 @@
   define("SERVER_NAME", "localhost");
   define("USER_NAME", "root");
   define("PASSWORD", "");
-  define("DB_NAME", "users");
+  define("DB_NAME", "tresorita");
 
   $conn = mysqli_connect(SERVER_NAME, USER_NAME, PASSWORD, DB_NAME);
 
