80e72c2807050488b2c780e139498f8480165629
Aidan Tomcy <aidantomcy@gmail.com>
2022-01-07 20:04:51 +0530

    Add Symfony\Dotenv dependency with composer

--- php/src/vendor/composer/autoload_static.php
+++ php/src/vendor/composer/autoload_static.php
@@ -7,6 +7,10 @@ namespace Composer\Autoload;
 class ComposerStaticInit20c4e6d13274965b680a914c100b8143
 {
     public static $prefixLengthsPsr4 = array (
+        'S' => 
+        array (
+            'Symfony\\Component\\Dotenv\\' => 25,
+        ),
         'P' => 
         array (
             'PHPMailer\\PHPMailer\\' => 20,
@@ -14,6 +18,10 @@ class ComposerStaticInit20c4e6d13274965b680a914c100b8143
     );
 
     public static $prefixDirsPsr4 = array (
+        'Symfony\\Component\\Dotenv\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/dotenv',
+        ),
         'PHPMailer\\PHPMailer\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
