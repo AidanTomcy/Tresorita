d78c0acb63efd0cd473d8dec6c054084228bf7ed
Aidan Tomcy <aidantomcy@gmail.com>
2022-01-24 19:41:09 +0530

    Moved MySQL Password to .env and loaded that

--- php/src/includes/dbh.inc.php
+++ php/src/includes/dbh.inc.php
@@ -1,8 +1,14 @@
 <?php
+  use Symfony\Component\Dotenv\Dotenv;
+  require '../vendor/autoload.php';
+
+  $dotenv = new Dotenv();
+  $dotenv->load(__DIR__.'/.env');
+
   # Database Connection
   define("SERVER_NAME", "localhost");
   define("USER_NAME", "root");
-  define("PASSWORD", "");
+  define("PASSWORD", $_SERVER['MYSQL_PASSWORD'] ?? "");
   define("DB_NAME", "tresorita");
 
   $conn = mysqli_connect(SERVER_NAME, USER_NAME, PASSWORD, DB_NAME);
