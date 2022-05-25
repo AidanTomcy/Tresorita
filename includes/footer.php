bb0fffd65702b1906e37e5d1cf811bccc616d3da
Aidan Tomcy <aidantomcy@gmail.com>
2022-05-25 07:42:17 +0530

    Add footer.php

---
+++ includes/footer.php
@@ -0,0 +1,98 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-lg-4 contact-box pt-1 d-md-block d-lg-flex d-flex">
+                <div class="contact-box__icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-phone-call" viewBox="0 0 24 24" stroke-width="1" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+                        <path d="M15 7a2 2 0 0 1 2 2" />
+                        <path d="M15 3a6 6 0 0 1 6 6" />
+                    </svg>
+                </div>
+                <div class="contact-box__info">
+                    <a href="tel:+919820184902" class="contact-box__info--title">+91 98201 84902</a>
+                    <p class="contact-box__info--subtitle"> Mon-Fri 9am-6pm</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 contact-box pt-1 d-md-block d-lg-flex d-flex">
+                <div class="contact-box__icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail-opened" viewBox="0 0 24 24" stroke-width="1" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <polyline points="3 9 12 15 21 9 12 3 3 9" />
+                        <path d="M21 9v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10" />
+                        <line x1="3" y1="19" x2="9" y2="13" />
+                        <line x1="15" y1="13" x2="21" y2="19" />
+                    </svg>
+                </div>
+                <div class="contact-box__info">
+                    <a href="mailto:josi@inoways.com" class="contact-box__info--title">josi@inoways.com</a>
+                    <p class="contact-box__info--subtitle">Online support</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 contact-box pt-1 d-md-block d-lg-flex d-flex">
+                <div class="contact-box__icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-2" viewBox="0 0 24 24" stroke-width="1" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <line x1="18" y1="6" x2="18" y2="6.01" />
+                        <path d="M18 13l-3.5 -5a4 4 0 1 1 7 0l-3.5 5" />
+                        <polyline points="10.5 4.75 9 4 3 7 3 20 9 17 15 20 21 17 21 15" />
+                        <line x1="9" y1="4" x2="9" y2="17" />
+                        <line x1="15" y1="15" x2="15" y2="20" />
+                    </svg>
+                </div>
+                <div class="contact-box__info">
+                    <a href="https://goo.gl/maps/Ssm6KJgKhTvPy57LA" class="contact-box__info--title" target="new">Tresorita Agro Farm</a>
+                    <p class="contact-box__info--subtitle">Asnas, Maharashtra</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-sm" style="background-color: #212121;">
+        <div class="container">
+            <div class="row py-4 text-center text-white">
+                <div class="col-lg-5 col-md-6 mb-4 mb-md-0">
+                    Connect with us on social media
+                </div>
+                <div class="col-lg-7 col-md-6">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="https://github.com/aidantomcy/tresorita-website" target="_blank"><i class="fab fa-github"></i></a>
+                    <a href="#"><i class="fab fa-linkedin"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container mt-5">
+        <div class="row text-white justify-content-center mt-3 pb-3">
+            <div class="col-12 col-sm-6 col-lg-6 mx-auto">
+                <h5 class="text-capitalize fw-bold">Tresorita</h5>
+                <hr class="bg-white d-inline-block mb-4" style="width: 60px; height: 2px;">
+                <p class="lh-lg">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d568.4124756812292!2d73.00473540999356!3d19.598543465370675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be70ba35ad54619%3A0x21d5a749607e0b4e!2stresorita%20Agrofarm!5e1!3m2!1sen!2sin!4v1629791512917!5m2!1sen!2sin" width="300" height="225" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </p>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-2 mb-4 mx-auto">
+                <h5 class="text-capitalize fw-bold">Useful links</h5>
+                <hr class="bg-white d-inline-block mb-4" style="width: 60px; height: 2px;">
+                <ul class="list-inline campany-list">
+                    <li><a href="images.php">Images</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="charityfund.php">Tresorita Charity Fund</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-2 mb-4 mx-auto">
+                <h5 class="text-capitalize fw-bold">Contact</h5>
+                <hr class="bg-white d-inline-block mb-4" style="width: 60px; height: 2px;">
+                <ul class="list-inline campany-list">
+                    <li><a href="mailto:josi@inoways.com">josi@inoways.com</a></li>
+                    <li><a href="mailto:sjosi0607@gmail.com">sjosi0607@gmail.com</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
