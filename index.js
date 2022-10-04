4fed3a7ce61c7327bae3bc71768514b33c81880b
Aidan Tomcy <aidantomcy@gmail.com>
2022-10-04 22:13:46 +0530

    Fix security vulnerabilities

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const helmet = require("helmet");
 const path = require("path");
+// deepcode ignore UseCsurfForExpress: <csurf package deprecated>
 const app = express();
 const port = 3000;
 
+app.disable("x-powered-by");
+app.use(helmet());
 app.use("/static", express.static(path.join(__dirname, "/website/public")));
 app.use("/", require("./routes"));
 
--- routes.js
+++ routes.js
@@ -4,15 +4,14 @@ const router = express.Router();
 
 const validRoutes = ["contact", "aboutus", "charityfund", "images"];
 
-router.get("/", (req, res) => {
+router.get("/", (_req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, "website/index.html"));
 });
 
-router.get("/:id", (req, res) => {
-  if (validRoutes.includes(req.params.id)) {
-    res
-      .status(200)
-      .sendFile(path.resolve(__dirname, `website/${req.params.id}.html`));
+router.get("/:id", (_req, res) => {
+  if (validRoutes.includes(_req.params.id)) {
+    const fileName = path.resolve(__dirname, `website/${_req.params.id}.html`);
+    res.status(200).sendFile(fileName);
   }
 });
 
